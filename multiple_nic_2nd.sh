25f2870e913fb841e58d98a85aaa14548fe26905
Cloud User <ec2-user@ip-172-31-31-131.us-east-2.compute.internal>
2018-12-28 16:44:32 +0000

    Changing the 2nd multiple nic file. Added code to start the 2nd interface if not already up. Tested on EC2 and works fine

--- multiple_nic_2nd.sh
+++ multiple_nic_2nd.sh
@@ -14,10 +14,12 @@
 # User inputs
 INTERFACE1="eth0"
 INTERFACE2="eth1"
-IP1=172.31.44.156/32
-IP2=172.31.39.104/32
-ROUTER1=172.31.32.1
-ROUTER2=172.31.32.1
+IP1=172.31.31.131/32
+IP2=172.31.27.130/32
+ROUTER1=172.31.16.1
+ROUTER2=172.31.16.1
+#INTERFACE2_UP="yes"
+INTERFACE2_UP="no"
 # End of user inputs
 
 if [[ $EUID != "0" ]]
@@ -31,9 +33,17 @@ yum install wget -y
 #### Make sure the file is available at this link
 rm -rf NetworkManager-dispatcher-routing-rules-1.12.0-8.el7_6.noarch.rpm
 wget https://rpmfind.net/linux/centos/7.6.1810/updates/x86_64/Packages/NetworkManager-dispatcher-routing-rules-1.12.0-8.el7_6.noarch.rpm
-yum install NetworkManager-dispatcher-routing-rules-1.12.0-8.el7_6.noarch.rpm   
+yum install NetworkManager-dispatcher-routing-rules-1.12.0-8.el7_6.noarch.rpm -y
 systemctl enable --now NetworkManager-dispatcher.service 
 rm -rf NetworkManager-dispatcher-routing-rules-1.12.0-8.el7_6.noarch.rpm
+
+# Bring the 2nd network card if not up
+if [[ $INTERFACE2_UP == "no" ]]
+then
+	nmcli connection delete test
+	nmcli connection add con-name test type ethernet ifname $INTERFACE2 autoconnect true
+	nmcli connection up test
+fi
 
 echo "from $IP1 table 1 priority 100" > /etc/sysconfig/network-scripts/rule-$INTERFACE1
 echo "from $IP2 table 2 priority 200" >> /etc/sysconfig/network-scripts/rule-$INTERFACE1
@@ -42,4 +52,6 @@ echo "default via $ROUTER1 dev $INTERFACE1 table 1" > /etc/sysconfig/network-scr
 echo "default via $ROUTER2 dev $INTERFACE2 table 2" >> /etc/sysconfig/network-scripts/route-$INTERFACE1
 
 echo "Network config changed successfully. Rebooting to make persistent changes"
+sleep 3
+reboot
 
